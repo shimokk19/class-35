2b7eaff9757a25e1142fc83b0d8ef5867ef9c86e
shimokk19 <73962569+shimokk19@users.noreply.github.com>
2021-03-19 19:10:32 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var hypnoticBall;
+var database, position;
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+
+    var hypnoticBallPosition = database.ref("ball/position");
+    hypnoticBallPosition.on("value",readPosition,showError);
+    //.on creates the listener 
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref("ball/position").set({//.set it sets the value
+        "x":position.x + x,
+        "y":position.y + y,
+    })
+    
+}
+function readPosition(data){position = data.val();//val = value
+hypnoticBall.x = position.x;
+hypnoticBall.y = position.y;
+};
+function showError(){
+    console.log("error");
+}
